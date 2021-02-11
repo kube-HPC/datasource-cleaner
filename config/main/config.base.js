5cc425e277abb861e4fe88cdf1ada9b19cbbe2b8
reggev <reggev.am@gmail.com>
2021-02-11 13:37:04 +0200

    cleanups

--- config/main/config.base.js
+++ config/main/config.base.js
@@ -4,11 +4,7 @@ const formatter = require(`${process.cwd()}/lib/utils/formatters`);
 
 const config = {};
 config.serviceName = packageJson.name;
-config.systemVersion = process.env.HKUBE_SYSTEM_VERSION;
 
-const secured = !!process.env.DATASOURCE_SERVICE_SSL;
-config.defaultStorage = process.env.DEFAULT_STORAGE || 's3';
-config.dvcStorage = process.env.DVC_STORAGE || 's3';
 config.clusterName = process.env.CLUSTER_NAME || 'local';
 config.keepDataSourceHotDuration = formatter.parseInt(
     process.env.KEEP_DATASOURCE_HOT_DURATION,
@@ -16,25 +12,9 @@ config.keepDataSourceHotDuration = formatter.parseInt(
 );
 
 config.version = packageJson.version;
-config.podName = process.env.POD_NAME;
 
 config.rest = {
-    port: process.env.DATASOURCE_SERVICE_REST_PORT || 3005,
     prefix: 'api',
-    poweredBy: 'HKube Server',
-    rateLimit: {
-        route: '/api',
-        ms: process.env.DATASOURCE_SERVICE_RATE_LIMIT_MS || 1000,
-        max: process.env.DATASOURCE_SERVICE_RATE_LIMIT_MAX || 5,
-        delay: process.env.DATASOURCE_SERVICE_RATE_LIMIT_DELAY || 0,
-    },
-};
-
-config.swagger = {
-    protocol: secured ? 'https' : 'http',
-    host: process.env.BASE_URL_HOST || 'localhost',
-    port: process.env.BASE_URL_PORT || config.rest.port,
-    path: process.env.BASE_URL_PATH || '',
 };
 
 config.db = {
@@ -52,8 +32,6 @@ config.db = {
 };
 
 config.fs = {
-    baseDirectory:
-        process.env.BASE_FS_ADAPTER_DIRECTORY || '/var/tmp/fs/storage',
     baseDatasourcesDirectory:
         process.env.BASE_DATASOURCES_DIRECTORY ||
         '/var/tmp/fs/datasources-storage',
--- tests/setup.js
+++ tests/setup.js
@@ -1,25 +1,9 @@
 const sinon = require('sinon');
 
-const DATASOURCE_GIT_REPOS_DIR = 'temp/git-repositories';
-
-const STORAGE_DIR = '/var/tmp/fs/storage/local-hkube-datasource';
-
 before(async () => {
     // eslint-disable-next-line
     const bootstrap = require('../bootstrap');
-    /** @type {import('./../lib/utils/types').config} */
-    const config = await bootstrap.init(true);
-    const baseUrl = `${config.swagger.protocol}://${config.swagger.host}:${config.swagger.port}`;
-    const restUrl = `${baseUrl}/${config.rest.prefix}/v1`;
-    const internalUrl = `${baseUrl}/internal/v1`;
-    global.testParams = {
-        restUrl,
-        internalUrl,
-        config,
-        DATASOURCE_GIT_REPOS_DIR,
-        STORAGE_DIR,
-        directories: config.directories,
-    };
+    await bootstrap.init(true);
 });
 
 afterEach(() => {
